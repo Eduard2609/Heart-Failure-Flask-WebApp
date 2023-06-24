7d787680be2aa37cb63e34fecfb11610386c1d7d
Eduard2609 <eduardolteanu@gmail.com>
2023-06-24 22:10:02 +0300

    restructure V1

---
+++ Flask_Heart/flaskheart/routes.py
@@ -0,0 +1,129 @@
+from flask import render_template, flash, redirect, url_for
+from flaskheart import app, db, bcrypt
+from flaskheart.forms import  SendToHeartApiForm, RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
+from datetime import datetime
+
+from tools.convert_data import convert_data
+from tools.connect_api import connect_to_api
+from tools.format import format_api_result
+from tools.date_csv import save_data_to_csv
+from flaskheart.models import User
+import csv
+import os
+
+
+#from flaskheart.models import User
+
+from tools.convert_data import convert_data
+from tools.connect_api import connect_to_api
+from tools.format import format_api_result
+import csv
+import os
+
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    form = SendToHeartApiForm()
+    if form.validate_on_submit():
+        # Process the form data and store it in a variable
+        user_data = {
+            'age': form.age.data,
+            'sex': form.sex.data,
+            'resting_bp': form.resting_bp.data,
+            'cholesterol': form.cholesterol.data,
+            'fasting_bs': form.fasting_bs.data,
+            'max_hr': form.max_hr.data,
+            'exercise_angina': form.exercise_angina.data,
+            'oldpeak': form.oldpeak.data,
+            'chest_pain_type': form.chest_pain_type.data,
+            'resting_ecg': form.resting_ecg.data,
+            'st_slope': form.st_slope.data,          
+            'model': form.model.data
+        }
+        
+
+        if current_user.is_authenticated:
+
+            # Save the user data to the user's CSV file that is called 'name' + 'surname' + '.csv'
+            csv_file = current_user.csv_file
+            save_data_to_csv(user_data, csv_file)
+            
+            # Convert the user data to a format that the Heart API can understand
+            user_data = convert_data(user_data)
+            # Connect to the Heart API and get the prediction
+            selected_model = form.model.data
+            prediction = connect_to_api(selected_model, user_data)
+            # Format the prediction
+            prediction = format_api_result(prediction)
+            #flash the prediction
+            if prediction is not None:
+                flash (f'{prediction}''%', 'success')
+            else:
+                flash (f'API request failed!', 'danger')
+    return render_template('home.html', form=form)
+
+
+def has_valid_history(current_user):
+    csv_file_path = os.path.join(app.instance_path, current_user.csv_file)
+    return os.path.isfile(csv_file_path)
+
+@app.route('/history')
+def history():
+    if not current_user.is_authenticated:
+        flash('Please login first!', 'danger')
+        return redirect(url_for('login'))
+    elif current_user.csv_file is None or not has_valid_history(current_user):
+        flash('No history available!', 'danger')
+        return redirect(url_for('home'))
+    else:
+        # Get the path to the user's CSV file
+        csv_path = os.path.join(app.instance_path, current_user.csv_file)
+        
+        # Read the user's CSV file
+        with open(csv_path, 'r', newline='') as csvfile:
+            # Create a CSV reader object
+            reader = csv.DictReader(csvfile)
+            
+            # Convert the CSV data to a list of dictionaries
+            data = [row for row in reader]
+    return render_template('history.html', data=data)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user= User(email=form.email.data, name=form.name.data, surname=form.surname.data, password=hashed_password, csv_file=form.name.data + form.surname.data + '.csv')
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Hey {form.name.data}! We succesfuly created an account created on {form.email.data}! You can log in now!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user =User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash(f'Hey {form.email.data}! You are logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccesful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('You are logged out!', 'success')
+    return redirect(url_for('home'))
---
+++ Flask_Heart/tools/date_csv.py
@@ -0,0 +1,30 @@
+import csv
+import os
+from flaskheart import app
+
+def save_data_to_csv(user_data, csv_file):
+    # Define the path of the CSV file
+    csv_path = os.path.join(app.instance_path, csv_file)
+    
+    # Define the fieldnames for the CSV file
+    fieldnames = ['age', 'sex', 'resting_bp', 'cholesterol', 'fasting_bs', 'max_hr', 'exercise_angina', 'oldpeak', 'chest_pain_type', 'resting_ecg', 'st_slope', 'model']
+    
+    # Check if the CSV file already exists
+    file_exists = os.path.isfile(csv_path)
+    
+    # Create the CSV file if it does not exist
+    if not file_exists:
+        with open(csv_path, 'w', newline='') as csvfile:
+            # Create a CSV writer object
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
+            # Write the header row to the CSV file
+            writer.writeheader()
+    
+    # Open the CSV file in append mode
+    with open(csv_path, 'a', newline='') as csvfile:
+        # Create a CSV writer object
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        # Write the user data to the CSV file
+        writer.writerow(user_data)
